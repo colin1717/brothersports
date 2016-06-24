72488089ff51b0996e7061e9a581e90bc4d96f4b
Colin Hancock <colin1717@mac.com>
2016-06-24 15:25:54 -0500

    routing working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,9 +9,25 @@ function MainRouter($stateProvider, $urlRouterProvider){
       url: '/',
       templateUrl: '../templates/home.html'
     })
+    .state('news', {
+      url: '/news',
+      templateUrl: '../templates/news.html'
+    })
+    .state('music', {
+      url: '/music',
+      templateUrl: '../templates/music.html'
+    })
     .state('shows', {
       url: '/shows',
       templateUrl: '../templates/shows.html'
+    })
+    .state('photos', {
+      url: '/photos',
+      templateUrl: '../templates/photos.html'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: '../templates/contact.html'
     });
 
     $urlRouterProvider.otherwise('/');
