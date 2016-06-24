47f6ad3cf527d38fda396eba4f79750e015b204b
Colin Hancock <colin1717@mac.com>
2016-06-24 17:30:05 -0500

    ng-repeat working

--- public/javascripts/ShowsController.js
+++ public/javascripts/ShowsController.js
@@ -6,8 +6,9 @@ angular
     var self = this;
 
     this.showList = [
-      {date: "July 16th", venue: "Barracuda", memo: "other bands playing"}
+      {date: "July 16th", venue: "Barracuda", memo: "Brother Sports and Yo Yo Ma"},
+      {date: "July 20th", venue: "Mohawk", memo: "Brother Sports, Slayer, and Tom Petty"}
     ];
 
-
+    return this;
   }
